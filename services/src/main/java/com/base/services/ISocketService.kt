a1ef0e47e988c8a92649bccbed95b04abeba82fb
dy <86118@163.com>
2026-03-14 19:21:50 +0800

    format & clean code

--- services/src/main/java/com/base/services/ISocketService.kt
+++
@@ -1,6 +0,0 @@
-package com.base.services
-
-interface ISocketService {
-    fun start()
-    fun stop()
-}
--- services/src/main/java/com/base/services/IToaster.kt
+++
@@ -1,5 +0,0 @@
-package com.base.services
-
-interface IToaster {
-    fun showToast(string: String)
-}
